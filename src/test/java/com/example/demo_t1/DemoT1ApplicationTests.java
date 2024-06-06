dfab7808cff112c983e4c48f7ce9116d2b7da842
LazySunIsMe <2825258839@qq.com>
2024-06-06 14:36:17 +0800

    Initial commit

---
+++ src/test/java/com/example/demo_t1/DemoT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoT1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
